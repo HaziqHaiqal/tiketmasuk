4766e2847028a03d6029c3d00ad26f8f27dccde9
HaziqHaiqal <haziqhaiqal1961@gmail.com>
2023-12-02 20:50:25 +0800

    intial commit

---
+++ vendor/kreativdev/installer/src/config/kdinstaller.php
@@ -0,0 +1,12 @@
+<?php 
+
+    return [
+        'item_name' => "Evento",
+        "item_id" => 45659949,
+        "vension" => 1.5,
+        "released_on" => "21th May, 2023",
+        "broadcast_driver" => "log",
+        "email_api" => 'https://kreativdev.com/emailcollector/api/collect'
+    ];
+
+?>
